f9ee9a5723a8455c7e3eedbb6ffbb06106a4fd19
SushantAgrawal <sagarwal@netwoven.com>
2019-05-05 22:18:19 +0530

    init

--- adam2-gql-server/resolvers.js
+++
@@ -1,9 +0,0 @@
-const db = require('./db');
-
-const Query = {
-    test: () => 'Test Success, GraphQL server is up & running !!',
-    greeting: () => 'Hello how are you?',
-    students: () => db.students.list(),
-    colleges: () => db.colleges.list()
-}
-module.exports = { Query }
